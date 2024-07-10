9b2d437df7c8a141684e2234253a3f785b2628f9
happyprogfrog <happyprogfrog@gmail.com>
2024-07-10 14:07:29 +0900

    feat: User 엔티티 생성
    - 리포지토리 추가
    - Auditing 기능 추가
    - 애플리케이션 실행 시 임시 데이터 생성 되도록 추가
    - application.yaml 수정

--- src/main/java/me/progfrog/cache_study/CacheStudyApplication.java
+++ src/main/java/me/progfrog/cache_study/CacheStudyApplication.java
@@ -1,13 +1,34 @@
 package me.progfrog.cache_study;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class CacheStudyApplication {
+@RequiredArgsConstructor
+@EnableJpaAuditing
+public class CacheStudyApplication implements ApplicationRunner {
+
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CacheStudyApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		userRepository.save(User.builder().name("frog01").email("a@a.com").build());
+		userRepository.save(User.builder().name("frog02").email("b@b.com").build());
+		userRepository.save(User.builder().name("frog03").email("c@c.com").build());
+		userRepository.save(User.builder().name("frog04").email("d@d.com").build());
+		userRepository.save(User.builder().name("frog05").email("e@e.com").build());
+		userRepository.save(User.builder().name("frog06").email("f@f.com").build());
+		userRepository.save(User.builder().name("frog07").email("g@g.com").build());
+		userRepository.save(User.builder().name("frog08").email("h@h.com").build());
+		userRepository.save(User.builder().name("frog09").email("i@i.com").build());
+		userRepository.save(User.builder().name("frog10").email("j@j.com").build());
+	}
 }
