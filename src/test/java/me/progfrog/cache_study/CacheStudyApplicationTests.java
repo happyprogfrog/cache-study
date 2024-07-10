ca6dddb905c1ff33168fc0df6456aefa69d73350
happyprogfrog <happyprogfrog@gmail.com>
2024-07-10 13:54:14 +0900

    프로젝트 생성

---
+++ src/test/java/me/progfrog/cache_study/CacheStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.cache_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
